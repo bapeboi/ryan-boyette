3d192a102573ccb16ab8a36b7ebdf42b657a46d7
Ryan Wesley Boyette <ryan.boyette@stu.jefferson.kyschools.us>
2018-03-13 09:26:41 -0400

    first commit

---
+++ ryans program.py
@@ -0,0 +1,18 @@
+fortnite = "fortnite"
+PUBG = "Pubg"
+favfortnite_skins = ["crackshot", "dark voyager", "rust lord", "alpine ace", "rapter", "merry marauder"]
+favfortnite_skins.append("brite bomber")
+
+
+pubg_fortnite = str(input ("which do you play pubg or fortnite?: "))
+if pubg_fortnite == fortnite:
+    print "fortnite is the best"
+elif pubg_fortnite == PUBG:
+    print "fortnite is gay" 
+print "a battle royale 100 person pvp type game where you build and fight to win"
+if pubg_fortnite == fortnite:    
+    print "fortnite"
+year_released = "july 25, 2017"
+print (year_released)
+
+    print ("my favorite fortnite skins are ") + str(favfortnite_skins)
