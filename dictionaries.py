c1e3d351ab852b45c51e7c1481765b0d8bb0d5ee
Ryan Wesley Boyette <ryan.boyette@stu.jefferson.kyschools.us>
2018-03-19 09:21:36 -0400

    commit

---
+++ dictionaries.py
@@ -0,0 +1,12 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> basketball_terms = {
+    'dribble' : "a way of advancing down the court by oneself.",
+    'foul' : "an infraction of the rules more serious than a violation.",
+    'rebound' : "a statistic awarded to a player who recieves the ball after a missed field goal or free throw.",
+    'triple double' : "having double digit number total in one of five statistical categories points, rebounds, and assists.",
+    'shooting' : "an attempt to score a basket by jumping, usually staight up , and in mid-jump, propelling the ball in an arc.",
+    'rpi' : "a quantity used to rank sports teams based upon a teams wins and losses and its strength of schedule.",
+    }
+for k, v in basketball_terms.items():
+    print k,v
