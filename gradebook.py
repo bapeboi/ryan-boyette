9ee3ec08e305d5ed68615c5a9093c4cc1cb4cad5
Ryan Wesley Boyette <ryan.boyette@stu.jefferson.kyschools.us>
2018-03-27 10:04:08 -0400

    commit

---
+++ gradebook.py
@@ -0,0 +1,12 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a = int(input("enter a grade here: " ))
+b = int(input("enter b grade here: " )) 
+c = int(input("enter c grade here: " ))
+d = int(input("enter d grade here: " ))
+e = int(input("enter e grade here: " ))
+
+total_sum = a+b+c+d+e
+
+average = total_sum/5
+print (average)
